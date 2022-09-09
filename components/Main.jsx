3bc72127a99b54a29dbd3a258d26c97e90c1ee1f
Dominik Czekański <83006539+czekens@users.noreply.github.com>
2022-09-09 22:54:41 +0200

    animation finished ufff

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,6 +3,7 @@ import styles from './Main.module.scss';
 // const Main = ({ display }) => {
 const Main = () => {
   // return <main className={`${styles.main} ${display ? 'visible' : 'invisible'}`}>Main sec</main>;
+  // style={{ display: 'none' }}
   return <main className={styles.main}>Main sec</main>;
 };
 
