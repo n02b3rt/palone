0f283945b15c6f535694008886569c18c633f2c2
Norbert Fxxxx <theonionman627@gmail.com>
2022-09-18 18:50:15 +0200

    Zastępstwa - design

--- components/Features.jsx
+++ components/Features.jsx
@@ -2,16 +2,21 @@ import styles from './Features.module.scss';
 import PhoneHeader from './UI/PhoneHeader';
 import PhoneFooter from './UI/PhoneFooter';
 import PhoneMain from './UI/PhoneMain';
+import Substitution from './UI/Substitution';
 // import './devices.css/dist/devices.css';
 
 const Features = ({ display }) => {
   return (
-    <section id='home' className={`${styles.features} ${display ? 'visible' : 'invisible'}`}>
-      <div className='device device-galaxy-s8'>
+    <section
+      id='home'
+      className={`${styles.features} ${display ? 'visible' : 'invisible'}`}
+    >
+      <div className={('device', 'device-galaxy-s8')}>
         <div className='device__sensors'></div>
         <div className='device__frame'>
           <div className={styles.phone}>
             <PhoneHeader />
+            <Substitution />
             <PhoneMain />
             <PhoneFooter />
           </div>
---
+++ components/UI/Substitution.jsx
@@ -0,0 +1,61 @@
+// import { SassNumber } from 'sass';
+import styles from './Substitution.module.scss';
+
+const Substitution = () => {
+  return (
+    <article className={styles.main}>
+      <nav className={styles.navbar}>
+        <div className={styles.navigation}>
+          <div className={styles.navigation__larrow}></div>
+        </div>
+        <div className={styles.navigation__head}>Zastępstwa na 2022-09-19</div>
+        <div className={styles.navigation}>
+          <div className={styles.navigation__rarrow}></div>
+        </div>
+      </nav>
+      {/*  */}
+      <div className={styles.substitiutution__item}>
+        <span className={styles.substitiutution__schlclass}>4dl</span>
+        <div className={styles.substitiutution__internal}>
+          <span className={styles.substitiutution__lesson}>(1)</span>
+          <span className={styles.substitiutution__content}>
+            ang1:jang &nbsp; A*** O*******, Anulowano
+            <span className={styles.substitiutution__room}></span>
+          </span>
+        </div>
+      </div>
+      {/*  */}
+      <div className={styles.substitiutution__item}>
+        <span className={styles.substitiutution__schlclass}>4eft</span>
+        <div className={styles.substitiutution__internal}>
+          <span className={styles.substitiutution__lesson}>2</span>
+          <span className={styles.substitiutution__content}>
+            jpol &rarr; matem &nbsp; E**** G**** &rarr; A*** W**** Zmień sale
+            lekcyjną: A24 &rarr; B04
+            <span className={styles.substitiutution__room}></span>
+          </span>
+        </div>
+      </div>
+      {/*  */}
+      <div className={styles.substitiutution__item}>
+        <span className={styles.substitiutution__schlclass}>4ftg</span>
+        <div className={styles.substitiutution__internal}>
+          <span className={styles.substitiutution__lesson}>3</span>
+          <span className={styles.substitiutution__content}>
+            1 Grupa: jangZ &rarr; WF &nbsp; A*** O******* &rarr; A****** Ł****
+            <span className={styles.substitiutution__room}></span>
+          </span>
+        </div>
+        <div className={styles.substitiutution__internal}>
+          <span className={styles.substitiutution__lesson}>6</span>
+          <span className={styles.substitiutution__content}>
+            jpol &nbsp; A*** K****** &rarr; E** K********
+            <span className={styles.substitiutution__room}></span>
+          </span>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default Substitution;
